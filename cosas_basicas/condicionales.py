9b01acdfbc012c4da76239fc74884338e131a8fa
DiegoV-bit <diegoandresvidalmunoz21@gmail.com>
2025-02-05 11:08:27 -0300

    Se sube una carpeta con algunos ejercicios basicos de Python

---
+++ cosas_basicas/condicionales.py
@@ -0,0 +1,7 @@
+a = 20 # Si se cambia el valor de a el programa va a la segunda opcion del programa
+
+# Se pregunta si el valor de a es 20
+if a == 20:
+    print("Es 20")
+else:
+    print("No es 20")
---
+++ cosas_basicas/variables.py
@@ -0,0 +1,7 @@
+a = 1 #El tipo de dato de a es entero (int)
+b = 2.0 #El tipo de dato de b es flotante (float)
+
+print(a) #Se imprime el valor de a
+print(b) #Se imprime el valor de b
+
+print(a+b) #Se imprime la suma de a y b dando como resultado 3.0
