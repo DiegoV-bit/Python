f07d028f8aaa4920a3d2c594c8b7b81c35dc1e51
DiegoV-bit <diegoandresvidalmunoz21@gmail.com>
2025-02-05 12:29:36 -0300

    Se agrega el programa donde se suman dos variables

--- cosas_basicas/suma.py
+++ cosas_basicas/suma.py
@@ -5,4 +5,5 @@ b = 1
 # Sumo las dos variables guardando el resultado en una nueva variable
 suma = a + b
 
+#se imprime el resultado guardado en la variable suma
 print("La suma de", a, "y", b, "es:", suma)
