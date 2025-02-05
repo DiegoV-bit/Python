0ccda594c49c51fac73cd388fc74232f451b6de6
DiegoV-bit <diegoandresvidalmunoz21@gmail.com>
2025-02-05 12:23:02 -0300

    Se agrega el programa para sumar variables

---
+++ cosas_basicas/suma.py
@@ -0,0 +1,8 @@
+# Declaro dos variables cualquiera en este caso son enteros
+a = 12
+b = 1
+
+# Sumo las dos variables guardando el resultado en una nueva variable
+suma = a + b
+
+print("La suma de", a, "y", b, "es:", suma)
