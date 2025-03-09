f7a401673e8c25ef40e97434c86f705748a7f70f
DiegoV-bit <diegoandresvidalmunoz21@gmail.com>
2025-03-09 17:46:18 -0300

    Se agrega la carpeta de estructuras de datos para python

---
+++ EDD/lista.py
@@ -0,0 +1,20 @@
+# listas
+
+lista = [39595, True, 2.71, "Rottweiler"]
+
+print(lista)
+
+# Imprimir cada uno de los elementos de la lista
+
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print(lista[3])
+# print(lista[4])
+
+# Imprimir con los indices negativos (esto imprime desde el ultimo elemento de la lista hasta el primero)
+print(lista[-1])
+print(lista[-2])
+print(lista[-3])
+print(lista[-4])
+
